a9bf7252fee585e84f97665220083a051368fae0
vieira-alexandre <alexandrer0x@hotmail.com>
2022-01-04 11:35:57 -0300

    first commit

---
+++ src/main/kotlin/dev/alexandrevieira/learningdynamodb/LearningDynamodbApplication.kt
@@ -0,0 +1,11 @@
+package dev.alexandrevieira.learningdynamodb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningDynamodbApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningDynamodbApplication>(*args)
+}
---
+++ src/test/kotlin/dev/alexandrevieira/learningdynamodb/LearningDynamodbApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.alexandrevieira.learningdynamodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningDynamodbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
